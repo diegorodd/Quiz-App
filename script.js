c4c0b1d6e1c8a3de4d75a2589585ce0a022712ac
diegorodd <dieegoa@icloud.com>
2022-05-31 17:09:51 -0400

    js

--- script.js
+++ script.js
@@ -30,6 +30,113 @@ const quizData = [
   const b_text = document.getElementById('b_text')
   const c_text = document.getElementById('c_text')
   const submitBtn = document.getElementById('submit')
+
+  var clock = document.getElementById('time')
+
+  var time = quizData.length * 15;
+  var timerId;
   
   let currentQuiz = 0 
   let score = 0 
+
+  loadQuiz()
+
+function loadQuiz() {
+
+    timerId = setInterval(clockStart, 1000);
+    clock.textContent = time;
+
+  deselectAnswers()
+
+  const currentQuizData = quizData[currentQuiz]
+
+  questionEL.textContent = currentQuizData.question
+  a_text.innerText = currentQuizData.a
+  b_text.innerText = currentQuizData.b
+  c_text.innerText = currentQuizData.c
+}
+
+function clockStart() {
+    time--;
+    clock.textContent = time;
+    if (time <= 0) {
+        clearInterval(timerId)
+    }
+}
+
+function deselectAnswers() {
+  answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+  let answer
+  answerEls.forEach(answerEl => {
+    if(answerEl.checked) {
+      answer = answerEl.id 
+    }
+})
+return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    if(answer) {
+      if(answer === quizData[currentQuiz].correct) {
+          console.log(answer)
+        score++
+      }
+      currentQuiz++
+
+      if(currentQuiz < quizData.length) {
+        loadQuiz()
+      } else {
+        quiz.innerHTML = `
+        <h2> You answered ${score}/${quizData.length} questions correctly </h2>
+
+        <button onclick="location.reload()">Reload</button>
+        ` 
+        clearInterval(timerId)
+        // time -= 15;
+        // clock.textContent = time;
+      }
+    }
+})
+
+var count = 0;
+// var time = 30;
+var marks = 0;
+var answer = [];
+// var timer;
+
+
+function buttons_manager(){
+  if(count > 0){
+    $(`#prev`).show();
+    if(count == 4){
+      $(`#next`).hide();
+      $(`#finish`).show();
+    }
+    else{
+      $(`#next`).show();
+    }
+  }
+  else{
+    $(`#prev`).hide();
+  }
+}
+
+console.log(answer)
+
+
+// function timer001() {
+//   c = c -1;
+//   if (c < 200) {
+//     time001.innerHTML = c;
+//   }
+
+//   if ( c < 1 ) {
+//     window.clearInterval(update);
+//   }
+// }
+
+// update = setInterval ("timer001()", 1000);
